c954d06e0396bcbeda481ca3112852f92c3adf14
John Chappell <aljopro@gmail.com>
2013-04-24 19:48:48 -0500

    Successful "angular"ation of a project

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('Weather.controllers', []).
+    controller('CurrentWeatherController', [function() {
+
+    //}])
+    //.controller('MyCtrl2', [function() {
+
+    }]);
